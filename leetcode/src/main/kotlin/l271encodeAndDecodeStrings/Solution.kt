4bdb267cb03e5cca1f3936bf2dc8a50287cc8e54
Ilya Buglakov <ilboogl@gmail.com>
2024-01-21 00:31:38 +0300

    feat(leetcode): add encode/decode problem solution

--- leetcode/src/main/kotlin/l271encodeAndDecodeStrings/Solution.kt
+++ leetcode/src/main/kotlin/l271encodeAndDecodeStrings/Solution.kt
@@ -2,48 +2,31 @@ package l271encodeAndDecodeStrings
 
 import java.util.LinkedList
 
-/**
- * WRONG AND UNFINISHED SOLUTION
- */
 class Solution {
 
     fun encode(strs: List<String>): String {
         val result = StringBuilder()
         for (i in strs.indices) {
-            result.append(strs[i].replace(delim, substitution))
-            if (i < strs.size - 1) {
-                result.append(delimChar)
-            }
+            result.append(strs[i].replace(FORBIDDEN_SEQUENCE, REPLACED))
+            result.append(DELIM)
         }
         return result.toString()
     }
 
     fun decode(str: String): List<String> {
+        if (str == DELIM) {
+            return listOf("")
+        }
         val answer: MutableList<String> = LinkedList()
-        var i = 0
-        val current = StringBuilder()
-        while (i < str.length) {
-            val next = str.indexOf(delimChar, i)
-            if (next == -1) {
-                current.append(str, i, str.length)
-                answer.add(current.toString())
-                return answer
-            } else if (str[i + 1] == delimChar) {
-                current.append(str, i, next + 1)
-                i = next + 1
-            } else {
-                current.append(str, i, next)
-                answer.add(current.toString())
-                current.setLength(0)
-                i = next
-            }
+        for (s in str.split(DELIM)) {
+            answer.add(s.replace(REPLACED, FORBIDDEN_SEQUENCE))
         }
         return answer
     }
 
     companion object {
-        private const val delim = "\\|"
-        private const val delimChar = '|'
-        private const val substitution = "||"
+        private const val DELIM = "~|~"
+        private const val FORBIDDEN_SEQUENCE = "~|"
+        private const val REPLACED = "~||"
     }
 }
--- leetcode/src/test/kotlin/l271encodeAndDecodeStrings/SolutionTest.kt
+++ leetcode/src/test/kotlin/l271encodeAndDecodeStrings/SolutionTest.kt
@@ -2,14 +2,24 @@ package l271encodeAndDecodeStrings
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
 
 class SolutionTest {
 
-    @Ignore
     @Test
     fun encodeThenDecodeTest() {
-        val source = listOf("ab", "v", "|", "ac|", "|d|", "||ee||||||");
+        val source = listOf("ab", "v", "~|~", "~|~",
+            "|", "ac|", "avav vvsd ~~~ sf ~|  sdf ~|~ asdfs ~||| asdf",
+            "~~||||~~~", "~|~", "~|~|~|~|~|||~||~~|~",
+            "|d|", "||ee||||||");
+        val actual = Solution().encode(source);
+        println(actual)
+        println(Solution().decode(actual))
+        assertEquals(source, Solution().decode(actual));
+    }
+
+    @Test
+    fun encodeEmptyTest() {
+        val source = listOf("");
         val actual = Solution().encode(source);
         println(actual)
         println(Solution().decode(actual))
